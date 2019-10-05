16f2e9f068c55260d51efe824abe7d0279beb453
Filip Meier <fimeier@student.ethz.ch>
2019-10-05 19:07:02 +0200

    backup

--- project/src/ch/ethz/netsec/fimeier/acme/runACME.java
+++ project/src/ch/ethz/netsec/fimeier/acme/runACME.java
@@ -18,6 +18,11 @@ import javax.net.ssl.SSLSocketFactory;
 import ch.ethz.netsec.fimeier.acme.dns.DNSServer;
 import ch.ethz.netsec.fimeier.acme.http.HTTPServer;
 
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
+
 public class runACME {
 
 	public static int shutdownHttpPort = 5003;
@@ -35,29 +40,29 @@ public class runACME {
 
 	public static String challengeType;
 	public static String dir, record, domain, revoke;
-	
+
 	//The acme-server's directory
 	private static URI dirUri = null;
 	private static Socket dirSocket = null;
 	private static PrintWriter dirSocketoutPrintWriter;
 	private static BufferedReader dirSocketinBufferedReader;
-	
+
 	private static void acmeClient() {
 		//get acme-server config aka directory
 		try {
 			//dirSocket = new Socket(dirUri.getHost(),dirUri.getPort());
-			
+
 			/*
-			
+
 			SSLSocketFactory ssf = (SSLSocketFactory) SSLSocketFactory.getDefault();
 			dirSocket = ssf.createSocket(dirUri.getHost(), dirUri.getPort());			
 			System.out.println(dirUri.getHost()+"=host "+dirUri.getPort()+"=port "+dirSocket.isConnected()+"=istconnected");
-			
-			
+
+
 			dirSocketoutPrintWriter = new PrintWriter(dirSocket.getOutputStream(), false);
 			dirSocketinBufferedReader = new BufferedReader(new InputStreamReader(dirSocket.getInputStream()));
-			*/
-			
+			 */
+
 			/*
 			dirSocketoutPrintWriter.println("GET / HTTP/1.1");
 			dirSocketoutPrintWriter.println("Host: www.badunetworks.com");
@@ -69,18 +74,20 @@ public class runACME {
 			// Add headers
 			dirACMEConnection.setRequestMethod("GET");
 			//dirACMEConnection.setRequestProperty(dir, "/dir");
-		
+
 			// Send data
 			dirACMEConnection.setDoOutput(false);
-		    //DataOutputStream outputStream = new DataOutputStream(dirACMEConnection.getOutputStream());
-		    
-		    //String message = "directory";
+			//DataOutputStream outputStream = new DataOutputStream(dirACMEConnection.getOutputStream());
+
+			//String message = "directory";
 			//byte[] resp = message.getBytes();
-			
-		    //outputStream.write(resp);
-		    //outputStream.flush();
-		    //outputStream.close();
-			BufferedReader in = new BufferedReader(new InputStreamReader(dirACMEConnection.getInputStream()));
+
+			//outputStream.write(resp);
+			//outputStream.flush();
+			//outputStream.close();
+			BufferedReader directoryResponse = new BufferedReader(new InputStreamReader(dirACMEConnection.getInputStream()));
+
+			/*
 			String inputLine;
 			StringBuffer response = new StringBuffer();
 
@@ -88,27 +95,81 @@ public class runACME {
 				response.append(inputLine);
 			}
 			in.close();
+			 */
+
+
+			JsonReader jsonReader = Json.createReader(directoryResponse);
+			JsonObject jobject = jsonReader.readObject();
+
+			/* https://tools.ietf.org/html/rfc8555#section-7.1.1
+			 HTTP/1.1 200 OK
+			   Content-Type: application/json
 			
-			String responseString = response.toString();
-		    System.out.println("responseString:\n"+responseString);
-		    dirACMEConnection.disconnect();
+			   {
+			     "newNonce": "https://example.com/acme/new-nonce",
+			     "newAccount": "https://example.com/acme/new-account",
+			     "newOrder": "https://example.com/acme/new-order",
+			     "newAuthz": "https://example.com/acme/new-authz",
+			     "revokeCert": "https://example.com/acme/revoke-cert",
+			     "keyChange": "https://example.com/acme/key-change",
+			     "meta": {
+			       "termsOfService": "https://example.com/acme/terms/2017-5-30",
+			       "website": "https://www.example.com/",
+			       "caaIdentities": ["example.com"],
+			       "externalAccountRequired": false
+			     }
+			   }
+						 */
+			URL newNonce = new URL(jobject.getString("newNonce"));
+			System.out.println("newNonce="+newNonce.toString());
+			
+			URL newAccount = new URL(jobject.getString("newAccount"));
+			System.out.println("newAccount="+newAccount.toString());
+			
+			URL newOrder = new URL(jobject.getString("newOrder"));
+			System.out.println("newOrder="+newAccount.toString());
+			
+			if (jobject.containsKey("newAuthz")) {
+				URL newAuthz = new URL(jobject.getString("newAuthz"));
+				System.out.println("newAuthz="+newAccount.toString());
+			}
+			else {
+				System.out.println("newAuthz= NOT AVAILABLE");
+			}
+			
+			URL revokeCert = new URL(jobject.getString("revokeCert"));
+			System.out.println("revokeCert="+newAccount.toString());
+
+			URL keyChange = new URL(jobject.getString("keyChange"));
+			System.out.println("keyChange="+newAccount.toString());
+
+
+
+			//String responseString = response.toString();
+			System.out.println("responseString:\n"+jobject.toString());
+			dirACMEConnection.disconnect();
 
 			//http://stackoverflow.com/a/15116323/4687348
 			//JsonParser jsonParser = new JsonParser();
 			//JsonObject object = (JsonObject)jsonParser.parse(responseString);
 
-		    //InputStream input = dirACMEConnection.getInputStream();
-		    //System.out.println(input.readAllBytes().toString());
-		     
-			
+			//InputStream input = dirACMEConnection.getInputStream();
+			//System.out.println(input.readAllBytes().toString());
+
+			/*
+			 * parse server config
+			 *
+			 */
+
+
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
-		
-		
+
+
+
+
 	}
 
 
@@ -165,17 +226,17 @@ public class runACME {
 
 
 	public static void main(String[] args) throws Exception {
-		
-		
-		
-		
-		
+
+
+
+
+
 		System.setProperty("javax.net.ssl.trustStore", acmeKeyStore);
 		System.setProperty("javax.net.ssl.trustStorePassword", "password");
-		
-		
-		
-		
+
+
+
+
 		System.out.println("Starting ACME-Project....");
 
 		parseArguments(args);
@@ -203,7 +264,7 @@ public class runACME {
 
 		System.out.println("Starting ACME-Client....");
 		acmeClient();
-		
+
 
 
 		System.out.println("Everything started....");
